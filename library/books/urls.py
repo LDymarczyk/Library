43515b2d9b884ac4fb155727cb3b12b1fa908d4d
laura.dymarczyk <aryadwen@gmail.com>
2018-09-14 12:07:16 +0200

    Created author detail view code

--- library/books/urls.py
+++ library/books/urls.py
@@ -3,10 +3,14 @@ from .views.authors_list import AuthorList
 from .views.books_list import BookList
 from .views.libraries_list import LibraryList
 from .views.rents_list import RentList
+from .views.users_list import UserList
+from .views.author_details import AuthorDetail
 
 urlpatterns = [
     path('authors/', AuthorList.as_view()),
     path('books/', BookList.as_view()),
     path('libraries/', LibraryList.as_view()),
     path('rents/', RentList.as_view()),
+    path('users/', UserList.as_view()),
+    path('authors/<int:pk>/', AuthorDetail.as_view()),
 ]
