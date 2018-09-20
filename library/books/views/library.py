7c921b096cd0304ea6581593f283fda0e84974ad
laura.dymarczyk <aryadwen@gmail.com>
2018-09-20 11:53:23 +0200

    Refactored library view

---
+++ library/books/views/library.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+from ..models.library import Library
+from ..serializers.library import LibrarySerializer
+from rest_framework import filters
+
+
+class LibraryViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Library.objects.all()
+    serializer_class = LibrarySerializer
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('address', 'name')
+    ordering_fields = ('address', 'name')
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
