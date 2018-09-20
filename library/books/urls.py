e7ef79c289519e28cf01184bb395abf9c95a2ed9
laura.dymarczyk <aryadwen@gmail.com>
2018-09-20 12:02:03 +0200

    Registered router in urls

--- library/books/urls.py
+++ library/books/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from .views.author import AuthorViewSet
+from .views.book import BookViewSet
+from .views.library import LibraryViewSet
+from .views.rent import RentViewSet
+from .views.user import UserViewSet
+
 
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet)
+router.register(r'books', BookViewSet)
+router.register(r'libraries', LibraryViewSet)
+router.register(r'rents', RentViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls))
