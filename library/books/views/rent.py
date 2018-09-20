689fd49d229116fa78430d45065082fa0a317d06
laura.dymarczyk <aryadwen@gmail.com>
2018-09-20 11:56:25 +0200

    Refactored rent view

---
+++ library/books/views/rent.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+from ..models.rent import Rent
+from ..serializers.rent import RentSerializer
+from rest_framework import filters
+
+
+class RentViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Rent.objects.all()
+    serializer_class = RentSerializer
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('book', 'reader', 'rent_id')
+    ordering_fields = ('rent_id', 'book')
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
