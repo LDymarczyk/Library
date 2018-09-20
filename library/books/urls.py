9e46ae5a04056cf6a9edb8bbeabb892129e6d11b
laura.dymarczyk <aryadwen@gmail.com>
2018-09-20 10:13:17 +0200

    Starting refactoring views

--- library/books/urls.py
+++ library/books/urls.py
@@ -12,19 +12,28 @@ from .views.library_details import LibraryDetail
 from .views.rent_details import RentDetail
 from .views.user_details import UserDetail
 from .views.author_filter import AuthorFilter
+from . import views
+# from .views.authors_list import AuthorListF
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'authors/', AuthorList)
 
 urlpatterns = [
     url(r'^$', api_root),
-    path('authors/', AuthorList.as_view(), name='authors-list'),
-    url('^authors/(?P<first_name>.+)/$', AuthorFilter.as_view()),
+    # path('authors/', AuthorList, name='authors-list'),
+    # url('^authors/(?P<first_name>.+)/$', AuthorFilter.as_view()),
     path('books/', BookList.as_view(), name='books-list'),
     path('libraries/', LibraryList.as_view(), name='libraries-list'),
     path('rents/', RentList.as_view(), name='rents-list'),
     path('users/', UserList.as_view(), name='users-list'),
-    path('authors/<int:pk>/', AuthorDetail.as_view(), name='author-details'),
+    # path('authors/<int:pk>/', AuthorDetail.as_view(), name='author-details'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-details'),
     path('libraries/<int:pk>/', LibraryDetail.as_view(), name='library-details'),
     path('rents/<int:pk>/', RentDetail.as_view(), name='rent-details'),
     path('users/<int:pk>/', UserDetail.as_view(), name='user-details'),
+    # url(r'^list$', AuthorListF.as_view())
 ]
+urlpatterns += router.urls
--- library/books/views/authors_list.py
+++ library/books/views/authors_list.py
@@ -3,20 +3,40 @@ from ..serializers.author import AuthorSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework import viewsets
 
-class AuthorList(APIView):
 
-    def get(self, request, format=None):
-        author=Author.objects.all()
-        serializer = AuthorSerializer(author, many = True)
-        return Response(serializer.data)
+class AuthorList(viewsets.ModelViewSet):
 
-    def post(self, request, format=None):
-        serializer = AuthorSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    # filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
+    # search_fields = ('first_name', 'last_name')
+
+    # def get_queryset(self):
+    #     return self.queryset
+
+    # def get(self, request, format=None):
+    #     author=Author.objects.all()
+    #     serializer = AuthorSerializer(author, many = True)
+    #     return Response(serializer.data)
+    #
+    # def post(self, request, format=None):
+    #     serializer = AuthorSerializer(data = request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
+
+
+# class AuthorListF(generics.ListAPIView):
+#     queryset = Author.objects.all()
+#     serializer = AuthorSerializer
+#     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
+#     search_fields = ('first_name', 'last_name')
+
