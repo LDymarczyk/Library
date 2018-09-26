cc1ee36e69beb396ef525933344392d316bab3a2
laura.dymarczyk <aryadwen@gmail.com>
2018-09-26 13:52:40 +0200

    Created a schemas for library validators

--- library/books/serializers/library.py
+++ library/books/serializers/library.py
@@ -3,6 +3,23 @@ from ..models.library import Library
 
 
 class LibrarySerializer(serializers.ModelSerializer):
+
+    def validator_email(self, email):
+
+        return email
+
+    def validator_address(self, address):
+
+        return address
+
+    def validator_address(self, name):
+
+        return name
+
+    def validator_telephone(self, telephone):
+
+        return telephone
+
     class Meta:
         model = Library
         fields = ('address', 'telephone', 'email', 'name')
