ed1e4975f532bf1f4455cec80407da50a367fb7c
laura.dymarczyk <aryadwen@gmail.com>
2018-09-26 13:58:58 +0200

    First step to library validators (not working)

--- library/books/serializers/library.py
+++ library/books/serializers/library.py
@@ -1,19 +1,22 @@
 from rest_framework import serializers
 from ..models.library import Library
+import re
 
 
 class LibrarySerializer(serializers.ModelSerializer):
 
     def validator_email(self, email):
-
+        if len(email) > 6:
+            if not re.match('\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b', email):
+                return 1
         return email
 
     def validator_address(self, address):
 
         return address
 
-    def validator_address(self, name):
-
+    def validator_name(self, name):
+        libraries = Library.objects.all()
         return name
 
     def validator_telephone(self, telephone):
