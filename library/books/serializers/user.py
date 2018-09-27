3a098af9bdbe28fd037f443eeabd74c6abf3080e
laura.dymarczyk <aryadwen@gmail.com>
2018-09-27 12:35:01 +0200

    Validation for PESEL in user serializer

--- library/books/serializers/user.py
+++ library/books/serializers/user.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from ..models.user import Reader
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    def validate_PESEL(self, PESEL):
+        if len(str(PESEL))!=11:
+            raise ValidationError("PESEL number must have 11 digits.")
+        control_sum = 0
+        control_numbers = [1,3,7,9,1,3,7,9,1,3]
+        for i in range(11):
+            control_sum += int(str(PESEL)[i]) * control_numbers[i]
+        if not control_sum % 10:
+            raise ValidationError("PESEL number is incorrect.")
+        return PESEL
+
     class Meta:
         model = Reader
         fields = ('first_name', 'last_name', 'birth_date', 'address', 'telephone')
