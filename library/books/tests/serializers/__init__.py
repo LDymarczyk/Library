afd30745f4c02b9a4b1a0ed8350189733949efb8
laura.dymarczyk <aryadwen@gmail.com>
2018-10-10 08:33:17 +0200

    Created first test for rent serializer

--- library/books/tests/serializers/__init__.py
+++ library/books/tests/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .author_serializer_tests import *
 from .book_serializer_tests import *
 from .library_serializer_tests import *
+from .rent_serializer_tests import *
---
+++ library/books/tests/serializers/rent_serializer_tests.py
@@ -0,0 +1,86 @@
+from rest_framework.test import APITestCase, APIRequestFactory
+from ...models import Author, Book, Library, Reader, Rent
+from ...serializers.rent import RentSerializer
+from rest_framework.exceptions import ValidationError, ErrorDetail
+from datetime import datetime, date
+
+def make_random_number():
+    today = datetime.today()
+    return str(today.hour) + str(today.min) + str(today.second) +str(today.microsecond)
+
+class RentSerializersTests(APITestCase):
+
+    def setUp(self):
+
+        self.user = Reader.objects.create_user(username='Ala',
+                                               email='mail@mail.com',
+                                               password='pass')
+
+        self.author = Author.objects.create(first_name='Jan',
+                                            last_name='Kowalski',
+                                            birth_year=1990,
+                                            death_year=2017,
+                                            creator=self.user)
+
+        self.author2 = Author.objects.create(first_name='Ala',
+                                             last_name='MaKota',
+                                             birth_year=1890,
+                                             death_year=1917,
+                                             creator=self.user)
+
+        self.library = Library.objects.create(address='ul. Klonowa 2, 40-000 Katowice',
+                                              email='bibl@mail.com',
+                                              name='Biblioteka ' + make_random_number(),
+                                              phone='123456789')
+
+        self.book = Book.objects.create(title='title1',
+                                        author=self.author,
+                                        ISBN=1234567890123,
+                                        genre='FS',
+                                        edition=1,
+                                        amount=12,
+                                        publication_date=2005,
+                                        publishing_house='FS',
+                                        status=True,
+                                        library=self.library
+                                        )
+
+        self.rent = Rent.objects.create(start_date=date(2018,10,8),
+                                        end_date=date(2018,11,8),
+                                        reader=self.user,
+                                        book=self.book)
+
+        self.factory = APIRequestFactory()
+
+    def test_contains_expected_fields_book_serializer(self):
+        data = RentSerializer(instance=self.rent)
+        self.assertCountEqual(data.fields, {'end_date',
+                                            'start_date',
+                                            'reader',
+                                            'book',
+                                            'library'
+                                            })
+
+    # def test_validate_with_correct_values(self):
+    #     data = {'address':"aaaaaaa",
+    #             'email':"email@email.com",
+    #             'name':"aaaaaaa"+make_random_number(),
+    #             'phone':"123456789"}
+    #     request = self.factory.get('/')
+    #     request.user = self.user
+    #     serializer = LibrarySerializer(data=data, context={'request': request})
+    #     self.assertTrue(serializer.is_valid())
+    #
+    # def test_validate_with_bad_email(self):
+    #     data = {'address':"aaaaaaa",
+    #             'email':"email",
+    #             'name':"aaaaaaa"+make_random_number(),
+    #             'phone':"123456789"}
+    #     request = self.factory.get('/')
+    #     request.user = self.user
+    #     serializer = LibrarySerializer(data=data, context={'request': request})
+    #     with self.assertRaises(ValidationError) as cm:
+    #         serializer.is_valid(raise_exception=True)
+    #     error = cm.exception.args[0]['email'][0]
+    #     self.assertEqual(ErrorDetail(string='Enter a valid email address.', code='invalid'), error)
+    #     self.assertFalse(serializer.is_valid())
