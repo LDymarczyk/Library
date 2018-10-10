5c1a4611522055c385dd22bb2763df3421f06e82
laura.dymarczyk <aryadwen@gmail.com>
2018-10-10 12:11:20 +0200

    Created tests for author view, one doesn't work

--- library/books/tests/views/author_view_tests.py
+++ library/books/tests/views/author_view_tests.py
@@ -27,6 +27,7 @@ class AuthorPermissionTest(APITestCase):
         self.author = Author.objects.create(**self.author_attrs)
 
         self.author_list_url = '/authors/'
+        self.author_detail_url = '/authors/{}/'.format(self.author.pk)
 
     def test_author_perform_create_method(self):
         client = APIClient()
@@ -34,3 +35,23 @@ class AuthorPermissionTest(APITestCase):
         response_create_author = client.post(self.author_list_url, self.author_attrs, format='json')
         self.assertEqual(response_create_author.status_code, 201)
         self.assertTrue(Author.objects.filter(first_name="Adam", creator=self.user).exists())
+
+    def test_author_perform_create_method_for_anonymous(self):
+        client = APIClient()
+        response_create_author = client.post(self.author_list_url, self.author_attrs, format='json')
+        self.assertEqual(response_create_author.status_code, 403)
+        self.assertFalse(Author.objects.filter(first_name="Adam", creator=self.user).exists())
+
+    def test_author_perform_update_method(self):
+        client = APIClient()
+        client.force_authenticate(self.user)
+        author_first_name = 'Krzysztof'
+        response_modify_author = client.patch(self.author_detail_url, {'first_name': author_first_name}, format='json')
+        self.assertEqual(response_modify_author.status_code, 200)
+
+    def test_author_perform_update_method_for_anonymous(self):
+        client = APIClient()
+        author_first_name = 'Franek'
+        response_modify_author = client.patch(self.author_detail_url, {'first_name': author_first_name}, format='json')
+        self.assertEqual(response_modify_author.status_code, 403)
+        self.assertFalse(Author.objects.filter(first_name="Franek").exists())
